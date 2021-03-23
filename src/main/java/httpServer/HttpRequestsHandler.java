5b6b8e35cde21fff3416dc2ba48a58266f827ace
Andrvat <andreivalitov2001@gmail.com>
2021-03-23 15:50:33 +0700

    Deleted unused code in comments

--- src/main/java/httpServer/HttpRequestsHandler.java
+++ src/main/java/httpServer/HttpRequestsHandler.java
@@ -15,24 +15,6 @@ import java.util.logging.Logger;
 public class HttpRequestsHandler implements Handler {
     private static final Logger logger = Logger.getLogger(ServerLauncher.class.getName());
 
-//    private static final ArrayList<String> SUPPORTED_METHODS = new ArrayList<>() {{
-//        add("GET");
-//    }};
-//
-//    private static final Map<String, String> CONTENT_TYPES = new LinkedHashMap<>() {{
-//        put("txt", "text/plain");
-//        put("html", "text/html");
-//        put("jpg", "image/jpeg");
-//        put("", "text/plain");
-//    }};
-//
-//    private static final Map<String, Integer> HEADERS_CODES = new LinkedHashMap<>() {{
-//        put("Not Found", 404);
-//        put("Method Not Allowed", 405);
-//        put("OK", 200);
-//        put("Not Acceptable", 406);
-//    }};
-
     private final int clientId;
 
     private InputStream inputStream;
